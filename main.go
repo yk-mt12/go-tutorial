646994ce48a76880634a0c23eef3d4217d008295
yk-mt12 <agvksl543@gmail.com>
2022-10-23 22:43:04 +0900

    Complated 19.関数

--- main.go
+++ main.go
@@ -5,7 +5,22 @@ import "fmt"
 func add (x, y int)(int, int){
 	return x + y, x-y
 }
+
+func cal(price, item int)(result int){
+	result = price * item
+	return
+}
+
+func convert(price int) float64{
+	return float64(price)
+}
+
 func main() {
 	r1, r2 := add(10, 20)
 	fmt.Println(r1, r2)
+
+	r3 := cal(100, 2)
+	fmt.Println(r3)
+
+	fmt.Println(convert(500))
 }
