5f7123d5b5fa8c5cb9c170bca158e1ff49b3b231
yk-mt12 <agvksl543@gmail.com>
2022-10-23 13:38:03 +0900

    コメントを追加

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	// varで定義した変数は関数の外でも宣言可能
 	var (
 		i int = 1
 	 f64 float64 = 1.2
@@ -10,4 +11,11 @@ func main() {
 	 t, f bool = true, false
 	)
 	fmt.Println(i, f64, s, t, f)
+
+	// 省略形は関数内でしか宣言できない
+	xi := 1
+	xf64 := 1.2
+	xs := "test"
+	xt, xf := true, false
+	fmt.Println(xi, xf64, xs, xt, xf)
 }
