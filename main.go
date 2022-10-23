27ebe22f5cc43a92c3af812d84c978a2fbdb58dd
yk-mt12 <agvksl543@gmail.com>
2022-10-23 13:27:46 +0900

    Deleted 不要な関数を削除

--- main.go
+++ main.go
@@ -2,20 +2,6 @@ package main
 
 import "fmt"
 
-/*
-一番最初に実行される特別な関数
-設定ファイルを記載する
-*/
-
-func init() {
-	fmt.Println("Init!")
-}
-
-func buzz() {
-	fmt.Println("Buzz")
-}
-
 func main() {
     fmt.Println("hello, world")
-		buzz()
 }
