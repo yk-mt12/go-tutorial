009041434b003374d82c90f9cea0b3be4b035041
yk-mt12 <agvksl543@gmail.com>
2022-10-23 14:58:05 +0900

    Completed 10.数値型（ビットシフト）

--- main.go
+++ main.go
@@ -4,6 +4,17 @@ import "fmt"
 
 // gofmt -w main.goでフォーマットを実行
 func main() {
-	x := 1 + 1
-	fmt.Println(x)
+	/*
+		x := 1 + 1
+		fmt.Println(x)
+	*/
+
+	// ビットシフト
+	// 1を0回シフトする
+	fmt.Println(1 << 0) // 0001 0001
+	// 1を1回シフトする
+	fmt.Println(1 << 1) // 0001 0010
+	fmt.Println(1 << 2) // 0001 0100
+	fmt.Println(1 << 3) // 0001 1000
+
 }
