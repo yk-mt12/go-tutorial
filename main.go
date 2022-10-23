bf022856ac4f688c6e440d418a2102dfe26c373a
yk-mt12 <agvksl543@gmail.com>
2022-10-23 13:25:41 +0900

    Added Init func and buzz fun

--- main.go
+++ main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+/*
+一番最初に実行される特別な関数
+設定ファイルを記載する
+*/
+
+func init() {
+	fmt.Println("Init!")
+}
+
+func buzz() {
+	fmt.Println("Buzz")
+}
+
 func main() {
     fmt.Println("hello, world")
+		buzz()
 }
