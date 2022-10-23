7a27795413065c6b65df03826262725549470f88
yk-mt12 <agvksl543@gmail.com>
2022-10-23 22:28:11 +0900

    lecture string

--- main.go
+++ main.go
@@ -2,19 +2,7 @@ package main
 
 import "fmt"
 
-// gofmt -w main.goでフォーマットを実行
 func main() {
-	/*
-		x := 1 + 1
-		fmt.Println(x)
-	*/
-
-	// ビットシフト
-	// 1を0回シフトする
-	fmt.Println(1 << 0) // 0001 0001
-	// 1を1回シフトする
-	fmt.Println(1 << 1) // 0001 0010
-	fmt.Println(1 << 2) // 0001 0100
-	fmt.Println(1 << 3) // 0001 1000
-
+	fmt.Println("Hello World")
+	fmt.Println("hello world"[0]) // [104]文字コード
 }
