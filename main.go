98c0fce95924a7b464a7cbc6124e0e7887544011
yk-mt12 <agvksl543@gmail.com>
2022-10-23 14:46:39 +0900

    Completed 9.const

--- main.go
+++ main.go
@@ -2,22 +2,12 @@ package main
 
 import "fmt"
 
-func main() {
-	// varで定義した変数は関数の外でも宣言可能
-	var (
-		i int = 1
-		// デフォルトでは、float64が入る。float32で定義したい場合には、明示的にfloat32型をつけておく
-	 f64 float64 = 1.2
-	 f32 float32 = 1.5
-	 s string = "test"
-	 t, f bool = true, false
-	)
-	fmt.Println(i, f64, f32, s, t, f)
+const Pi = 3.14
 
-	// 省略形は関数内でしか宣言できない
-	xi := 1
-	xf64 := 1.2
-	xs := "test"
-	xt, xf := true, false
-	fmt.Println(xi, xf64, xs, xt, xf)
+const (
+	username = "test user"
+	password = "test password"
+)
+func main() {
+	fmt.Println(Pi, username, password)
 }
