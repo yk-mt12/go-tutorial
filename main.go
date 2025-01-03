e3a2605db235082f196f4944dcf846c8d353cabf
Yuki Matsuishi <yuki.matsuishi@zozo.com>
2025-01-03 12:17:29 +0900

    5. // flag.Args関数を使用し、フラグ以外の引数を取得

--- main.go
+++ main.go
@@ -13,6 +13,13 @@ func init() {
 	flag.IntVar(&n, "n", 1, "回数")
 }
 func main() {
+	// flagで指定された値分、msgを出力
 	flag.Parse()
 	fmt.Println(strings.Repeat(*msg, n))
+
+	// flag.Args関数を使用し、フラグ以外の引数を取得
+	args := flag.Args()
+	if len(args) > 0 {
+		fmt.Println(args)
+  }
 }
