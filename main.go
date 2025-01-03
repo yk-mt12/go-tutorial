4e1e8da17ddfcb3901d74c6a9e4c74d0152b9b55
Yuki Matsuishi <yuki.matsuishi@zozo.com>
2025-01-03 12:21:56 +0900

    5. 標準出力とエラー出力、終了コード

--- main.go
+++ main.go
@@ -2,24 +2,10 @@ package main
 
 import (
 	"fmt"
-	"flag"
-	"strings"
+	"os"
 )
 
-var msg = flag.String("msg", "hoge", "Flag to print a message")
-var n int
-
-func init() {
-	flag.IntVar(&n, "n", 1, "回数")
-}
 func main() {
-	// flagで指定された値分、msgを出力
-	flag.Parse()
-	fmt.Println(strings.Repeat(*msg, n))
-
-	// flag.Args関数を使用し、フラグ以外の引数を取得
-	args := flag.Args()
-	if len(args) > 0 {
-		fmt.Println(args)
-  }
+	fmt.Fprintln(os.Stderr, "error")
+	os.Exit(1)
 }
