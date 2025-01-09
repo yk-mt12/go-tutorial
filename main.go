465d30097febded833c31d8c97db877963d70baf
Yuki Matsuishi <yuki.matsuishi@zozo.com>
2025-01-09 21:57:25 +0900

    10. リクエストパラメーターの使用

--- main.go
+++ main.go
@@ -1,33 +1,34 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 )
+
+type Person struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	 // リクエストパラメータを取得
+	 name := r.FormValue("name")
+	 if name == "" {
+			 name = "ゲスト"
+	 }
+
+	 // 運勢をランダムに選択
+	 omikuji := []string{"大吉", "中吉", "小吉", "凶"}
+	 result := omikuji[0] // ここでは固定で "大吉" を返すようにしています
+
+	 // レスポンスを生成
+	 response := fmt.Sprintf("%sさんの運勢は「%s」です！", name, result)
+	 fmt.Fprintln(w, response)
+}
 
 func main() {
 
-	type Person struct {
-		Name string `json:"name"`
-		Age  int    `json:"age"`
-	}
-
-	p := Person{Name: "John", Age: 30}
-
-	var buf bytes.Buffer
-	enc := json.NewEncoder(&buf)
-	if err := enc.Encode(p); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(buf.String())
-
-	var p2 Person
-	dec := json.NewDecoder(&buf)
-	if err := dec.Decode(&p2); err!= nil {
-		log.Fatal(err)
-	}
-	fmt.Println(p2)
+	http.HandleFunc("/", handler)
+  log.Fatal(http.ListenAndServe(":8080", nil))
 }
